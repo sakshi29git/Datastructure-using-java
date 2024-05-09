4d4d2a4ed9893ba5a84d067ed66e35691011dc2e
Sakshi <sakshinagare137@gmail.com>
2024-05-09 17:53:28 +0530

    Create Greaternum3.java

---
+++ Greaternum3.java
@@ -0,0 +1,46 @@
+package MyDs;
+import java.util.Scanner;
+
+public class Greaternum3 {
+	public static void main(String[] args) {
+		int num1,num2,num3,max;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number1:");
+		num1=sc.nextInt();
+		
+		System.out.println("Enter number2:");
+		num2=sc.nextInt();
+		
+		System.out.println("Enter number3:");
+		num3=sc.nextInt();
+		
+		
+		if(num1>num2)
+		{
+			if(num1>num3)
+			{
+				System.out.println("Number 1 is greater...");
+			}
+			else {
+				System.out.println("Number 3 is greater...");
+			}
+		}
+		else {
+			if(num2>num3)
+			{
+				System.out.println("Number 2 is greater...");
+			}
+			else 
+			{
+				System.out.println("Number 3 is greater...");
+			}
+		}
+
+	}
+}
+
+/*Total time complexity-total conditional statement is 1
+ * so time complexity is 
+ * 
+ * /
+ */
